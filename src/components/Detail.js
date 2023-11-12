c9f733cf693851af273c58b5210328e7f46f245b
DESKTOP-HB03G6R\Thai Thong <nguyenthaithong98@gmail.com>
2023-11-12 21:28:09 +0900

    Change Content

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -15,7 +15,7 @@ function Detail(params) {
     const navigate = useNavigate();
 
     // https://docs.google.com/spreadsheets/d/1GMmK95MPBJdFGFvKLz-LTPvDvjMcfRmWb_mmIp5Yrz0/edit#gid=0
-    const endpoint = 'https://script.google.com/macros/s/AKfycbyWmj_G0Vwd9N_fMMLpFhFYuDqAXsEDkIcESJDTPJnTxUXpQfmET30A3DQQzcrzSvz67Q/exec?id=1'
+    const endpoint = 'https://script.google.com/macros/s/AKfycbyDO7clO6GPbA3P3WxLipeFBo56HpIZz8Woa5zAINKq_b3WXoI7Wd7Rt9Q2ytZmefREUA/exec'
 
 
     useEffect(() => {
@@ -28,7 +28,7 @@ function Detail(params) {
 
         if (isLoading) {
             try {
-                axios.get(endpoint).then(response => {
+                axios.get(endpoint, { params: { id: id } }).then(response => {
                     if (response.status === 200) {
                         console.log(response.data);
                         setData(response.data);
@@ -69,9 +69,19 @@ function Detail(params) {
                                                 </div>
                                                 <div className="col-6">
                                                     <div className="alert alert-primary" role="alert" style={{ textAlign: 'left', height: '100%' }}>
-                                                        <p>Nhóm: </p>
-                                                        <p>Chu kì </p>
-                                                        <p>Phân lớp </p>
+                                                        <div className="row">
+                                                            <div className="col-6">
+                                                                <p>Nhóm: </p>
+                                                                <p>Chu kì </p>
+                                                                <p>Phân lớp </p>
+                                                            </div>
+                                                            <div className="col-6">
+                                                                <p>{data ? data.Nhom : 'Đang cập nhật'}</p>
+                                                                <p>{data ? data.ChuKi : 'Đang cập nhật'}</p>
+                                                                <p>{data && data.PhanLop ? data.PhanLop : '###'}</p>
+                                                            </div>
+                                                        </div>
+
                                                     </div>
                                                 </div>
                                             </div>
@@ -89,11 +99,10 @@ function Detail(params) {
                                                     <div className="col-4">
                                                         <p>{data ? data.SoHieuNguyenTu : ''}</p>
                                                         <p>{data ? data.NguyenTuKhoi : ''}</p>
-                                                        <p>xx </p>
-                                                        <p>xx</p>
-                                                        <p>xx</p>
-                                                        <p>xx</p>
-                                                        <p>xx</p>
+                                                        <p>{data && data.SoKhoi ? data.SoKhoi : '###'}</p>
+                                                        <p>{data && data.DanhMuc ? data.DanhMuc : '###'}</p>
+                                                        <p>{data && data.MauSac ? data.MauSac : '###'}</p>
+                                                        <p>{data && data.CoTinhPhongXa ? data.CoTinhPhongXa : '###'}</p>
                                                     </div>
                                                 </div>
 
@@ -110,17 +119,17 @@ function Detail(params) {
                                             <div className="alert alert-primary" role="alert" style={{ textAlign: 'left', overflow: 'hidden', height: 'auto' }}>
 
                                                 <div className="row">
-                                                    <div className="col-10">
+                                                    <div className="col-8">
                                                         <p>Số electron mỗi phân lớp</p>
                                                         <p>Cấu hình electron</p>
                                                     </div>
-                                                    <div className="col-2">
-                                                        <p>xx</p>
+                                                    <div className="col-4">
+                                                        <p>{data && data.SoElectronMoiPhanLop ? data.SoElectronMoiPhanLop : '###'}</p>
                                                         <p dangerouslySetInnerHTML={{ __html: data ? data.CauHinhE : '' }} />
                                                     </div>
                                                 </div>
-                                                <div className="mt-3">
-                                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="-800 -320 1600 640" className="atom pt-6" width={'100%'}>
+                                                <div style={{ color: 'black' }} className="mt-3" dangerouslySetInnerHTML={{ __html: data && data.HinhAnh ? data.HinhAnh : '###' }}>
+                                                    {/* <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="-800 -320 1600 640" className="atom pt-6" width={'100%'}>
                                                         <g className="g">
                                                             <circle className="nucleus" r="130"></circle>
                                                             <text className="font-fam" x="0" y="0" fill="#ffffff" alignmentBaseline="middle" dominantBaseline="middle" textAnchor="middle">He</text>
@@ -130,7 +139,7 @@ function Detail(params) {
                                                                 <circle className="electron" cx="0" cy="230" r="25"></circle>
                                                             </g>
                                                         </g>
-                                                    </svg>
+                                                    </svg> */}
                                                 </div>
                                             </div>
 
@@ -148,13 +157,13 @@ function Detail(params) {
                                                         <p>Nhiệt dung</p>
                                                     </div>
                                                     <div className="col-4">
-                                                        <p>xx</p>
-                                                        <p>xx</p>
-                                                        <p>xx </p>
-                                                        <p>xx</p>
-                                                        <p>xx</p>
-                                                        <p>xx</p>
-                                                        <p>xx</p>
+                                                        <p>{data && data.TrangThaiVatChat ? data.TrangThaiVatChat : '###'}</p>
+                                                        <p>{data && data.MatDo ? data.MatDo : '###'}</p>
+                                                        <p>{data && data.NhietDoNongChay ? data.NhietDoNongChay : '###'} </p>
+                                                        <p>{data && data.NhietDoSoi ? data.NhietDoSoi : '###'}</p>
+                                                        <p>{data && data.NhietLuongNongChay ? data.NhietLuongNongChay : '###'}</p>
+                                                        <p>{data && data.NhietBayHoi ? data.NhietBayHoi : '###'}</p>
+                                                        <p>{data && data.NhietDung ? data.NhietDung : '###'}</p>
                                                     </div>
                                                 </div>
 
@@ -179,13 +188,13 @@ function Detail(params) {
                                                         <p>Trạng thái ôxy hóa</p>
                                                     </div>
                                                     <div className="col-4">
-                                                        <p>xx</p>
-                                                        <p>xx</p>
-                                                        <p>xx </p>
+                                                        <p>{data && data.BanKinhNguyenTu ? data.BanKinhNguyenTu : '###'}</p>
+                                                        <p>{data && data.BanKinhCongHoaTri ? data.BanKinhCongHoaTri : '###'}</p>
+                                                        <p>{data && data.DoAmDien ? data.DoAmDien : '###'} </p>
                                                         <p>{data ? data.NangLuongIonHoa : ''}</p>
                                                         <p>{data ? data.NguyenTuKhoi : ''}</p>
-                                                        <p>xx</p>
-                                                        <p>xx</p>
+                                                        <p>{data && data.DoDanNhiet ? data.DoDanNhiet : '###'} </p>
+                                                        <p>{data && data.TrangThaiOxyHoa ? data.TrangThaiOxyHoa : '###'} </p>
                                                     </div>
                                                 </div>
                                             </div>
@@ -194,10 +203,10 @@ function Detail(params) {
                                                 <p className="headerOutline">ỨNG DỤNG </p>
                                                 <p>{data ? data.UngDung : ''}</p>
                                             </div>
-                                            <div className="alert alert-primary" role="alert" style={{ textAlign: 'left', overflow: 'hidden' }}>
+                                            {/* <div className="alert alert-primary" role="alert" style={{ textAlign: 'left', overflow: 'hidden' }}>
                                                 <p className="headerOutline">SỐ ĐỒNG VỊ </p>
                                                 <p></p>
-                                            </div>
+                                            </div> */}
                                         </div>
                                     </div>
                                 </div>
